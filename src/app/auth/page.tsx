733ea3beaaef5cbfdf975004632351aba5c25b18
elguapo87 <pglogovac@gmail.com>
2025-11-08 15:40:17 +0100

    Created PostCard component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,6 +4,7 @@ import Loading from "@/components/Loading";
 import StoriesBar from "@/components/StoriesBar"
 import { dummyPostsData } from "../../../public/assets";
 import { useEffect, useState } from "react";
+import PostCard from "@/components/PostCard";
 
 type FeedType = typeof dummyPostsData;
 
@@ -30,7 +31,9 @@ const Feed = () => {
         <StoriesBar />
 
         <div className="p-4 space-y-6">
-          List of posts
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
 
