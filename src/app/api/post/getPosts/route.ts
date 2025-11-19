b6162347ded7561a198bbc2662d690af153cdd23
elguapo87 <pglogovac@gmail.com>
2025-11-19 18:42:07 +0100

    Created getPosts API

---
+++ src/app/api/post/getPosts/route.ts
@@ -0,0 +1,29 @@
+import { protectUser } from "@/middleware/userAuth";
+import postModel from "@/models/postModel";
+import userModel from "@/models/userModel";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const { authorized, user: authUser } = await protectUser();
+        if (!authorized || !authUser) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+
+        const user = await userModel.findById(authUser._id);
+
+        // User connections & followings
+        const userIds = [...new Set([authUser._id, ...user.connections, ...user.following])];
+
+        const posts = await postModel.find({ user: { $in: userIds } }).populate("user").sort({ createdAt: -1 });
+
+        return NextResponse.json({ success: true, posts });
+
+    } catch (error) {
+        console.error("Add post error:", error);
+        return NextResponse.json(
+            { success: false, message: "Failed to get posts" },
+            { status: 500 }
+        );
+    }
+}
