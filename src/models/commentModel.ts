8acd00a23ce88e2229f44455f8c42e7670eec16c
elguapo87 <pglogovac@gmail.com>
2025-12-15 22:32:52 +0100

    Created commentModel

---
+++ src/models/commentModel.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    user_id: { type: String, ref: "user", required: true },
+    post_id: { type: String, ref: "post", required: true },
+    text: { type: String }
+}, { timestamps: true });
+
+const commentModel = mongoose.models.comment || mongoose.model("comment", commentSchema);
+
+export default commentModel;
