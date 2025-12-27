ae3cfb3919fecdf41c6de211e543c5c270275567
elguapo87 <pglogovac@gmail.com>
2025-12-27 20:25:31 +0100

    Adjusted clerk middleware for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Apply Clerk to everything EXCEPT /api/sse/*
+//     '/((?!api\/sse).*)',
+//   ],
+// };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Apply Clerk to everything EXCEPT /api/sse/*
-    '/((?!api\/sse).*)',
+    // Exclude SSE and Clerk webhooks
+    "/((?!api/sse|api/webhooks/clerk).*)",
   ],
 };
