5b4084b3138d110599b7f895d295ea26885caa34
elguapo87 <pglogovac@gmail.com>
2025-12-28 23:38:25 +0100

    Testing clerk middleware for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,44 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Apply Clerk to everything EXCEPT /api/sse/*
-//     '/((?!api\/sse).*)',
-//   ],
-// };
-
-// import { clerkMiddleware } from '@clerk/nextjs/server';
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Apply Clerk to everything EXCEPT /api/sse/*
-//     '/((?!api\/sse).*)',
-//   ],
-// };
-
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isPublicRoute = createRouteMatcher([
-  "/api/webhooks/clerk(.*)",
-  "/api/inngest(.*)",
-  "/api/sse(.*)",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return;
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
+    // Apply Clerk to everything EXCEPT /api/sse/*
+    '/((?!api\/sse).*)',
   ],
 };
+
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Apply Clerk to everything EXCEPT /api/sse/*
+//     '/((?!api\/sse).*)',
+//   ],
+// };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher([
+//   "/api/webhooks/clerk(.*)",
+//   "/api/inngest(.*)",
+//   "/api/sse(.*)",
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+// ]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isPublicRoute(req)) return;
+// });
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|.*\\..*).*)",
+//   ],
+// };
 
 
 
