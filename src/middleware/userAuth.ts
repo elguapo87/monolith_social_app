3bff2184d83cfc65c031ecb8520e5c57430205f4
elguapo87 <pglogovac@gmail.com>
2026-01-24 19:31:04 +0100

    Refactored code in AuthGuard component & in protectUser middleware

--- src/middleware/userAuth.ts
+++ src/middleware/userAuth.ts
@@ -2,28 +2,6 @@ import connectDB from "@/config/db";
 import userModel from "@/models/userModel";
 import { auth } from "@clerk/nextjs/server";
 
-// export const protectUser = async () => {
-//     try {
-//         const { userId } = await auth();
-//         if (!userId) {
-//             return { authorized: false, user: null };
-//         }
-
-//         await connectDB();
-
-//         const user = await userModel.findById(userId);
-//         if (!user) {
-//             return {  authorized: false, user: null }; 
-//         }
-
-//         return { authorized: true, user }
-
-//     } catch (error) {
-//         console.error("protectUser error:", error);
-//         return { authorized: false, user: null };
-//     }
-// };
-
 export const protectUser = async () => {
     try {
         const { userId } = await auth();
