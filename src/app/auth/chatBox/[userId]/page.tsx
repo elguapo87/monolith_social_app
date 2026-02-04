d3794b5db5a02b3a212f36b7caace535f7a058f0
elguapo87 <pglogovac@gmail.com>
2026-02-05 00:42:23 +0100

    Created getConnectionUser thunk & used in ChatBox page

--- src/app/auth/chatBox/[userId]/page.tsx
+++ src/app/auth/chatBox/[userId]/page.tsx
@@ -8,78 +8,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
 import { useParams } from "next/navigation";
 import { useAuth } from "@clerk/nextjs";
-import api from "@/lib/axios";
-import toast from "react-hot-toast";
 import { addMessage, fetchMessages, resetMessages } from "@/redux/slices/messageSlice";
-
-type User = {
-    _id: string;
-    full_name: string;
-    profile_picture?: string;
-    user_name?: string;
-    bio?: string;
-}
+import { getConnectionUser } from "@/redux/slices/connectionSlice";
 
 const ChatBox = () => {
     const { userId } = useParams() as { userId: string };
 
+    const currentUser = useSelector((state: RootState) => state.user.value);
     const messages = useSelector((state: RootState) => state.message.messages);
-    const { getToken } = useAuth();
+    const connectionUser = useSelector((state: RootState) => state.connection.connectionUser);
     const dispatch = useDispatch<AppDispatch>();
+    const { getToken } = useAuth();
 
     const [text, setText] = useState("");
     const [image, setImage] = useState<File | null>(null);
-    const [user, setUser] = useState<User | null>(null);
 
     const messageEndRef = useRef<HTMLDivElement | null>(null);
 
-
-    const fetchConnection = async () => {
-        try {
-            const token = await getToken();
-            const { data } = await api.post("/connection/getConnection", { otherUserId: userId }, {
-                headers: { Authorization: `Bearer ${token}` }
-            });
-
-            if (data.success) {
-                setUser(data.user);
-
-            } else {
-                toast.error(data.message);
-            }
-
-        } catch (error) {
-            const err = error instanceof Error ? error.message : "Unknown error";
-            console.error(err);
-        }
-    }
-
-    const fetchUserMessages = async () => {
-        try {
-            const token = await getToken();
-            dispatch(fetchMessages({ to_user_id: userId, token }));
-
-        } catch (error) {
-            const errMesage = error instanceof Error ? error.message : "An unknown error occurred";
-            toast.error(errMesage)
-        }
-    };
-
     useEffect(() => {
-        const init = async () => {
-            try {
-                fetchConnection();
-                dispatch(resetMessages());
-                fetchUserMessages();
-
-
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        init();
-    }, [userId]);
+        getToken().then((token) => {
+            dispatch(getConnectionUser({ otherUserId: userId, token }));
+            dispatch(resetMessages());
+            dispatch(fetchMessages({ to_user_id: userId, token }));
+        });
+    }, [getToken, dispatch, userId]);
 
     //  Auto-scroll to most recent message
     useEffect(() => {
@@ -87,51 +39,40 @@ const ChatBox = () => {
     }, [messages]);
 
     const sendMessage = async () => {
+        if (!text.trim() && !image) return;
+
+        const token = await getToken();
         try {
-            const token = await getToken();
-
-            if (!text.trim() && !image) return;
-
             const formData = new FormData();
             formData.append("id", userId);
             formData.append("text", text);
             image && formData.append("image", image);
 
-            const { data } = await api.post("/message/addMessage", formData, {
-                headers: { Authorization: `Bearer ${token}` }
-            });
-
-            if (data.success) {
-                setText("");
-                setImage(null);
-                dispatch(addMessage(data.message));
-
-            } else {
-                toast.error(data.message);
-            }
+            dispatch(addMessage({ messageData: formData, token }));
+            setText("");
+            setImage(null);
 
         } catch (error) {
-            const errMesage = error instanceof Error ? error.message : "An unknown error occurred";
-            console.error(errMesage);
+            console.error("Failed to add message");
         }
     };
 
-    return user && (
+    return connectionUser && (
         <div className="flex flex-col h-screen">
             <div
                 className="flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-linear-to-r from-indigo-50
                     to-purple-50 border-b border-gray-300"
             >
                 <Image
-                    src={user.profile_picture || assets.avatar_icon}
+                    src={connectionUser.profile_picture || assets.avatar_icon}
                     alt=""
                     width={32}
                     height={32}
                     className="size-8 rounded-full"
                 />
                 <div>
-                    <p className="font-medium">{user.full_name}</p>
-                    <p className="text-sm text-gray-500 -mt-1.5">@{user.user_name}</p>
+                    <p className="font-medium">{connectionUser.full_name}</p>
+                    <p className="text-sm text-gray-500 -mt-1.5">@{connectionUser.user_name}</p>
                 </div>
             </div>
 
@@ -139,31 +80,37 @@ const ChatBox = () => {
                 <div className="space-y-4 max-w-4xl mx-auto">
                     {messages.toSorted((a, b) =>
                         new Date(a.createdAt ?? "").getTime() - new Date(b.createdAt ?? "").getTime())
-                        .map((message, index) => (
-                            <div
-                                key={index}
-                                className={`flex flex-col ${message.to_user_id !== user._id
-                                    ? "items-start" : "items-end"}`}
-                            >
+                        .map((message, index) => {
+                            const isMine = message.from_user_id.toString() === currentUser?._id.toString();
+
+                            return (
                                 <div
-                                    className={`p-2 text-sm max-w-sm bg-white text-slate-700 rounded-lg
-                                        shadow ${message.to_user_id !== user._id
-                                            ? "rounded-bl-none" : "rounded-br-none"}`}
+                                    key={index}
+                                    className={`flex flex-col ${isMine
+                                        ? "items-start" : "items-end"}`}
                                 >
-                                    {message.message_type === "image" && (
-                                        <Image
-                                            onClick={() => window.open(message.media_url)}
-                                            src={message.media_url ?? ""}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                            className="w-full max-w-sm rounded-lg mb-1 cursor-pointer"
-                                        />
-                                    )}
-                                    <p>{message.text}</p>
+                                    <div
+                                        className={`p-2 text-sm max-w-sm bg-white text-slate-700 rounded-l shadow                             
+                                            ${isMine
+                                                ? "rounded-bl-none" 
+                                                : "rounded-br-none"}`}
+                                    >
+                                        {message.message_type === "image" && (
+                                            <Image
+                                                onClick={() => window.open(message.media_url)}
+                                                src={message.media_url ?? ""}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                                className="w-full max-w-sm rounded-lg mb-1 cursor-pointer"
+                                            />
+                                        )}
+                                        <p>{message.text}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            )
+
+                        })}
 
                     <div ref={messageEndRef} />
                 </div>
